ab799b5f1d0fff2e1972461f252665e3adb6c259
NgachaIan <ngacha.duncan@students.jkuat.ac.ke>
2019-01-17 03:59:04 +0300

    [finishes #163077002] create test for endpoint allowing an admin to create a meetup record

---
+++ tests/test_meetup.py
@@ -0,0 +1,25 @@
+from app import create_app
+import unittest
+import os
+import sys
+import json
+import requests
+topdir = os.path.join(os.path.dirname(__file__), "../")
+sys.path.append(topdir)
+
+class CreateMeetup(unittest.TestCase):
+    def test_create_meetup(self):
+        with create_app().test_client() as c:
+            url = "/api/version1/meetup/post"
+            meetup = {
+                "topic": "django",
+                "location": "strath",
+                "Tags": "pythondjango"
+            }
+            response = c.post(url, data=json.dumps(meetup), headers={
+                              "Content-Type": "application/json"})
+            expected = json.loads(response.get_data())
+            self.assertEqual(response.status_code, 201)
+
+if __name__ == "__main__":
+    unittest.main()
